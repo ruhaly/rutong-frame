0027fe26c42598a97bfed0975e0de518db92a39a
ruhaly <hlr36hlr>
2016-08-16 17:29:40 +0800

    初始化导入

---
+++ app/src/main/java/com/mvp/rutong/frame/base/BaseFragment.java
@@ -0,0 +1,45 @@
+package com.mvp.rutong.frame.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mvp.rutong.frame.base.util.TUtil;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by han_l on 2016/8/16.
+ */
+
+public abstract class BaseFragment<P extends BasePresenter, M extends BaseModel> extends Fragment {
+    public P mPresenter;
+    public M mModel;
+    public Context mContext;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), container, false);
+        ButterKnife.bind(this, view);
+        mContext = getActivity().getBaseContext();
+        mPresenter = TUtil.getT(this, 0);
+        mModel = TUtil.getT(this, 1);
+        this.initView();
+        this.initPresenter();
+        return view;
+    }
+
+    public abstract int getLayoutId();
+
+    public abstract void initView();
+
+    /**
+     * 简单页面无需mvp就不用管此方法即可,完美兼容各种实际场景的变通
+     */
+    public abstract void initPresenter();
+}

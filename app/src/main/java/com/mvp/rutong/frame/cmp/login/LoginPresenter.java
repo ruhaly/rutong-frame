8b9c42d3484c005b27dd06cc9287dc4393d45a67
ruhaly <hlr36hlr>
2016-08-16 18:09:18 +0800

    初始化导入

--- app/src/main/java/com/mvp/rutong/frame/cmp/login/LoginPresenter.java
+++ app/src/main/java/com/mvp/rutong/frame/cmp/login/LoginPresenter.java
@@ -8,17 +8,12 @@ import com.mvp.rutong.frame.base.util.MD5Util;
 /**
  * Created by han_l on 2016/5/31.
  */
-public class LoginPresenter extends BasePresenter<LoginContract.Model, LoginContract.View>{
+public class LoginPresenter extends BasePresenter<LoginContract.Model, LoginContract.View> {
     public void login(String name, String pass) {
         mRxManage.add(mModel.login(name, MD5Util.encode(pass).toUpperCase()).subscribe(user -> {
             mView.loginSuccess();
             mView.showMsg("登录成功");
             mRxManage.post(C.EVENT_LOGIN, user.getUserNickName());
         }, throwable -> mView.showMsg("登录失败")));
-    }
-
-    @Override
-    public void onStart() {
-
     }
 }
